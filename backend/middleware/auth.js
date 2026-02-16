4bef6db1fcdb18e47fe3eecb9c6f92f0cd1acd4b
purushotham0114 <229x1a33b3@gprec.ac.n>
2026-02-16 13:31:25 +0530

    changes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -2,7 +2,7 @@ export const requireAuth = (req, res, next) => {
   console.log("session id ", req.session.userId)
   if (!req.session.userId) {
     console.log("in authcheck")
-    return res.status(299).json({ message: 'Authentication required' });
+    return res.status(401).json({ message: 'Authentication required' });
   }
   console.log("after the session check")
   next();
